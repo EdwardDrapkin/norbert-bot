0566012b3f9d68abc0bb457a3dffbfe02842d6bb
Edward Drapkin <edward@colorjar.com>
2016-05-02 08:03:34 -0400

    Wolfram|Alpha plugin

---
+++ src/plugins/WolframAlphaPlugin.js
@@ -0,0 +1,51 @@
+// @flow
+
+import SimpleChanMsgPlugin from 'plugins/SimpleChanMsgPlugin';
+import Norbert from 'lib/Norbert';
+import WolframAlpha from 'wolfram-alpha';
+
+export default class WolframAlphaPlugin extends SimpleChanMsgPlugin {
+    client:WolframAlpha;
+
+    constructor(apiKey:string) {
+        super();
+        this.client = WolframAlpha.createClient(apiKey);
+    }
+
+    getName() {
+        return "WolframAlpha";
+    }
+
+    getHelp() {
+        return {
+            overview: "Query Wolfram|Alpha",
+            commands: {
+                wa: "query - do a search.",
+            }
+        }
+    }
+
+    getCommands() {
+        return {
+            wa: this.alpha.bind(this)
+        }
+    }
+
+    alpha(channel:string, sender:string, message:string, norbert:Norbert) {
+        this.client.query(message, function(err, results) {
+            if(err != null) {
+                norbert.client.say(channel, "error");
+                console.log(err)
+            }
+
+            for(let result of results) {
+                if(result.hasOwnProperty('title') && result.title == 'Result'
+                    && result.hasOwnProperty('subpods') &&
+                        result.subpods.length > 0
+                ) {
+                    norbert.client.say(channel, `<Wolfram|Alpha> ${result.subpods[0].text}`);
+                }
+            }
+        });
+    }
+}
