3697361070dcb9155f52c64420962b2c57a404d9
Edward Drapkin <edward@colorjar.com>
2016-05-05 10:25:15 -0400

    even more dice

--- src/plugins/DiceRollerPlugin.js
+++ src/plugins/DiceRollerPlugin.js
@@ -29,16 +29,33 @@ export default class DiceRollerPlugin extends SimpleChanMsgPlugin {
 
     roll(channel:string, sender:string, message:string, norbert:Norbert) {
         let sides = 20;
+        let dice = 1;
 
         message = message.trim();
 
         if(message != "") {
             let num = message.split(/\s+/)[0];
-            sides = Number.parseInt(num);
+            if(num.match(/d/)) {
+                let nums = num.split('d');
+                dice = Number.parseInt(nums[0]);
+                sides = Number.parseInt(nums[1]);
+
+                let results = [];
+                for(let i = 0; i < dice; i++) {
+                    results.push(Math.floor(Math.random() * (sides - 1)) + 1);
+                }
+
+                norbert.client.say(channel, `${sender}, I rolled you ${dice} d${sides}s: [${results.join(', ')}]`);
+            } else {
+                sides = Number.parseInt(num);
+
+                let result = Math.floor(Math.random() * (sides - 1)) + 1;
+                norbert.client.say(channel, `${sender}, I rolled you a d${sides}: ${result}`);
+            }
         }
 
-        let result = Math.floor(Math.random() * (sides - 1)) + 1;
 
-        norbert.client.say(channel, `${sender}, I rolled you a d${sides}: ${result}`);
+
+
     }
 }
