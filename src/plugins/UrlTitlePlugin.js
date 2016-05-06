3479dcfcecc826ebf4d6436d7bd3a56fa01e6910
Edward Drapkin <edward@colorjar.com>
2016-05-06 05:54:27 -0400

    mirror mirror on the wall who has teh most overengineered title plugin of them all

--- src/plugins/UrlTitlePlugin.js
+++ src/plugins/UrlTitlePlugin.js
@@ -2,7 +2,10 @@
 
 import SimpleChanDaemonPlugin from 'plugins/SimpleChanDaemonPlugin';
 import Norbert from 'lib/Norbert';
-import scrape from 'html-metadata';
+import request from 'request';
+import cheerio from 'cheerio';
+import filesize from 'filesize';
+import imageSize from 'image-size';
 
 export default class UrlTitlePlugin extends SimpleChanDaemonPlugin {
     timeout:Number;
@@ -23,10 +26,45 @@ export default class UrlTitlePlugin extends SimpleChanDaemonPlugin {
     }
 
     getUrlTitle(channel:string, sender:string, message:string, norbert:Norbert, triggered:string) {
-        scrape(triggered, (err, results) => {
-            if(!err && results.hasOwnProperty('general') && results.general.hasOwnProperty('title')) {
-                norbert.client.say(channel, `<${sender}> ${results.general.title.trim()}`)
-            }
+        let contentType = '';
+        let size = 0;
+        let humanSize = '';
+        let image = {};
+
+        request({method: 'HEAD', uri: triggered}, (error, headResponse, headBody) => {
+            contentType = headResponse.headers['content-type'];
+            size = headResponse.headers['content-length'] || 0;
+            humanSize = filesize(size);
+
+            let buffer;
+            request(triggered, (error, response, body) => {
+                if(!error) {
+                    if(contentType.startsWith('text/')) {
+                        let title = cheerio.load(buffer.toString())('title').text().trim();
+                        if(title != "") {
+                            norbert.client.say(channel, `<${sender}> ${title}`);
+                        }
+                    } else if(contentType.startsWith('image/')) {
+                        let image = imageSize(buffer);
+                        let message = `<${sender}> ` +
+                            `${image.height}x${image.width} ${image.type} - ${humanSize}`;
+
+                        norbert.client.say(channel, message);
+                    } else {
+                        let message = `<${sender}> ` +
+                            `${contentType} - ${humanSize}`;
+                        norbert.client.say(channel, message);
+                    }
+                }
+            }).on('response', response => {
+                response.on('data', (chunk) => {
+                    buffer = Buffer.concat(buffer ? [buffer, chunk] : [chunk]);
+
+                    if(buffer.length > 1024 * 5) { //we've fetched a few kb
+                        response.destroy();
+                    }
+                });
+            });
         });
     }
 
