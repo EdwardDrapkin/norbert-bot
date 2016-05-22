6a41503e4dc2eef8be14427437e1a08f774d5d49
Edward Drapkin <edward@colorjar.com>
2016-05-22 17:26:01 -0400

    more tests

--- src/__tests__/QuotePlugin.test.js
+++ src/__tests__/QuotePlugin.test.js
@@ -1,17 +1,33 @@
-import QuotePlugin from '../plugins/QuotePlugin'
+import QuotePlugin from '../plugins/QuotePlugin';
 import expect from 'expect.js';
 
 describe("Quote plugin", () => {
     it("Tests removal of various timestamp patterns", () => {
-       let message = "<foo> is a bar";
-       let inputs = [
-           "<00:00> ",
-           "00:00 - ",
-           "[[00:00]] "
-       ];
+        let messages = [
+            "<foo> is a bar",
+            "foo> is a bar",
+            "foo is a bar",
+            "foo: is a bar",
+            "[foo] is a bar",
+            "    foo is a bar",
+            "    foo is a <00:00> bar",
+        ];
+        let inputs = [
+            "<00:00>",
+            "00:00",
+            "00:00:00",
+            "20160522 01:46:32",
+            "0522@01:46",
+            "00:00 -",
+            "[[00:00]]"
+        ];
 
-       for(let input of inputs) {
-           expect(QuotePlugin.stripTimestamps(input+message)).to.eql(message);
-       }
-   });
+        for(let message of messages) {
+            for(let input of inputs) {
+                const test = `${input} ${message}`;
+                expect(QuotePlugin.stripTimestamps(test)).to.eql(message.trim());
+            }
+        }
+
+    });
 });
--- src/plugins/QuotePlugin.js
+++ src/plugins/QuotePlugin.js
@@ -1,9 +1,21 @@
+import SimpleChanMsgPlugin from 'plugins/SimpleChanMsgPlugin';
+
 export default class QuotePlugin {
     static stripTimestamps(input) {
-        return input
-            .replace(/(\d{2}:?)\d{2}/, '')
-            .replace(/\W+(\W\w)/, '$1')
-            .trim();
+        return input.replace(/^(?:\W*\d*?\W*)*(\W\w)/, '$1').trim();
     }
 
+    getName() {
+        return "Quote";
+    }
+
+    getHelp() {
+        return {
+            overview: "just a quote plugin, y'all.",
+            commands: {
+                quote: "fetch a quote",
+                addquote: "[quote] - add a quote"
+            }
+        }
+    }
 }
